c4a972ceca4c963b3522cb072ed94090598b8fd4
xuyuanzhou <635043894@qq.com>
2018-05-15 15:58:10 +0800

    Update RCTVLCPlayer.h

--- ios/RCTVLCPlayer/RCTVLCPlayer.h
+++ ios/RCTVLCPlayer/RCTVLCPlayer.h
@@ -1,8 +1,16 @@
-#import "RCTView.h"
+#import "React/RCTView.h"
 
 @class RCTEventDispatcher;
 
 @interface RCTVLCPlayer : UIView
+
+@property (nonatomic, copy) RCTBubblingEventBlock onVideoProgress;
+@property (nonatomic, copy) RCTBubblingEventBlock onVideoPaused;
+@property (nonatomic, copy) RCTBubblingEventBlock onVideoStopped;
+@property (nonatomic, copy) RCTBubblingEventBlock onVideoBuffering;
+@property (nonatomic, copy) RCTBubblingEventBlock onVideoPlaying;
+@property (nonatomic, copy) RCTBubblingEventBlock onVideoEnded;
+@property (nonatomic, copy) RCTBubblingEventBlock onVideoError;
 
 - (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
 
